8ad529b8984944304ce39420a1d64bc048733730
coreyt-gremlin <55258574+coreyt-gremlin@users.noreply.github.com>
2023-08-28 15:05:51 -0400

    Revert "cloudflair fix"
    This reverts commit 219f66c4f4e60ad8951c4784d7030675786091a3.

--- astro.config.js
+++ astro.config.js
@@ -2,18 +2,14 @@ import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig, sharpImageService } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
   integrations: [mdx(), prefetch(), tailwind()],
   experimental: {
-    assets: true
+    assets: true,
   },
-  output: "server",
-  adapter: cloudflare()
 });
